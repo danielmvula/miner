631ce1364561433fc9ef5ad6b8269f26d427ebfd
danielmvula <89077621+danielmvula@users.noreply.github.com>
2021-12-07 11:12:10 +0200

    Update troncoin.sh

--- troncoin.sh
+++ troncoin.sh
@@ -2,4 +2,5 @@
 sudo apt update
 wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.28/lolMiner_v1.28a_Lin64.tar.gz
 tar -xf lolMiner_v1.28a_Lin64.tar.gz
+cd 1.28a/
 ./lolMiner --algo ETHASH --pool ethash.unmineable.com:3333 --user TRX:TRY324CXCMCcjWrxWK8b6pgtsaPCiphu9D.workerdmvula7.colab --ethstratum ETHPROXY
